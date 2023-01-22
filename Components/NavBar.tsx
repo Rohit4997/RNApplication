80686bf029542707dc466c4235cc93df57d3224f
Rohit Kumar Dhaker <rohit.dhaker@juspay.in>
2023-01-22 16:55:09 +0530

    Added Screen And Component Folder

---
+++ Components/NavBar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text, View} from 'react-native';
+
+
+const NavBar = () => {
+    return(
+        <View>
+            <Text>Navbar</Text>
+        </View>
+    );
+}
+
+export default NavBar;
---
+++ InitApp.tsx
@@ -0,0 +1,17 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import Home from './Screens/Home';
+ 
+ function InitApp(): JSX.Element {
+   return (
+    <Home/>
+   );
+ }
+
+ export default InitApp;
